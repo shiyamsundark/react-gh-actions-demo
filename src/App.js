cad9dc105f148f8f126e04e1001471c643e54d66
shiyamsundark <105621600+shiyamsundark@users.noreply.github.com>
2022-12-27 18:02:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         <form>
          <div>
            <label for="version">Release_Version</label>
-           <input type="text" name="Release-Version" id="version" placeholder="Enter Artifact's Version">
+           <input type="text" name="Release-Version" id="version" placeholder="Enter Artifact's Version" required />
          </div> 
          <div>
            <label for="modeltype">Model_Type</label>
-           <select name="" id="">
+           <select name="" id="" />
            <option value="">Hyundai</option>
            <option value="">Kia</option>
            <option value="">MG</option>
