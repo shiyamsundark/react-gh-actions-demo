a4e4a1b1a9671363f23a84b7b445ad4e27ef87da
shiyamsundark <105621600+shiyamsundark@users.noreply.github.com>
2022-12-27 15:14:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Data Extractor Form
         </p>
+        <form>
+         <div className="input-container">
+           <label for="releaseversion">Release Version </label>
+           <input type="text" name="release-version" id="releaseversion" placeholder="Enter Artifact's Releaseversion" required />
+         </div>
+         <div className="input-container">
+           <label for="">Model Type </label>
+           <select name="" id="">
+           <option value="">Tata</option>
+           <option value="">Hyundai</option>
+           <option value="">Honda</option>
+         </div>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Now, it's been changed automatically... Thanks to github actions...
+          <button onClick={handleClick}>BUILD</button>
         </a>
       </header>
     </div>
   );
 }
-
-export default App;
