4bb27e5321e7a4752804b392a543c7d2893e4e2e
shiyamsundark <105621600+shiyamsundark@users.noreply.github.com>
2023-04-04 12:35:44 +0530

    Update indexv1.js

--- indexv1.js
+++ indexv1.js
@@ -5,6 +5,6 @@ exports.handler = async (event, context) => {
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
-    return event.key2;  // Echo back the first key value
+    return event.key1;  // Echo back the first key value
     // throw new Error('Something went wrong');
 };
